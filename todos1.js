c6dcf6c20b4579776531d550d99fbee6b98bf31a
simei <simei@simei-no-MacBook-Air-4.local>
2016-03-21 15:40:41 +0900

    initialize

--- todos1.js
+++ todos1.js
@@ -1,47 +1,13 @@
+// Tasks = new Mongo.Collection("tasks");
+
+// Tasks.insert({
+//   owner: Meteor.userId(),
+//   createdAt: new Date(),
+//   title: "task1"
+// });
+
 if (Meteor.isClient) {
-    // counter starts at 0
-    Session.setDefault('counter', 0);
-
-    Template.hello.helpers({
-	counter: function () {
-	    return Session.get('counter');
-	}
-    });
-
-    Template.hello.events({
-	'click button': function () {
-	    // increment the counter when button is clicked
-	    Session.set('counter', Session.get('counter') + 1);
-	}
-    });
-
-    Template.nametag.helpers({
-	// name: "Ben Bitdiddle"
-    });
-
-    Template.welcomePage.helpers({
-	people: [{name: "Bob"}, {name: "Frank"}, {name: "Alice"}]
-    });
-
-    Template.profilePage.helpers({
-	username: function(){
-	    return Meteor.user() && Meteor().username;
-	}
-    });
-
-    Template.post.helpers({
-	commentCount: function (numComments) {
-	    if (numComments === 1) {
-		return "1 comment";
-	    } else {
-		return numComments + " comments";
-	    }
-	}
-    });
 }
 
 if (Meteor.isServer) {
-    Meteor.startup(function () {
-	// code to run on server at startup
-    });
 }
