a298055ca4ca01dd3356fc130529c1e42666b090
simei <simei@simei-no-MacBook-Air-4.local>
2016-03-21 17:35:43 +0900

    %4 Remote tassk form

--- taskestimate1.js
+++ taskestimate1.js
@@ -5,17 +5,32 @@ if (Meteor.isClient) {
 	passwordSignupFields: "USERNAME_ONLY"
     });
     if (Meteor.userId()){
-	Tasks.insert({
-	    owner: Meteor.userId(),
-	    createdAt: new Date(),
-	    title: "task1"
-	});
 	Template.body.helpers({
 	    tasks: function () {
 		return Tasks.find({});
 	    }
 	});
     }
+    Template.body.events({
+	"submit .new-task": function (event) {
+	    event.preventDefault();
+	    var text = event.target.text.value;
+	    Tasks.insert({
+		owner: Meteor.userId(),
+		createdAt: new Date(),
+		title: text
+	    });
+	    event.target.text.value = "";
+	},
+	"click .toggle-checked": function () {
+	    Tasks.update(this._id, {
+		$set: {checked: ! this.checked}
+	    });
+	},
+	"click .delete": function () {
+	    Tasks.remove(this._id);
+	}
+    });
 }
 
 if (Meteor.isServer) {
