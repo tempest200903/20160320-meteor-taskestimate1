33c98807fd5f927e4e358bad6620e921be30f1d7
simei <simei@simei-no-MacBook-Air-4.local>
2016-03-21 17:15:32 +0900

    %2 Collection Tasks

--- taskestimate1.js
+++ taskestimate1.js
@@ -1,15 +1,21 @@
-// Tasks = new Mongo.Collection("tasks");
-
-// Tasks.insert({
-//     owner: Meteor.userId(),
-//     createdAt: new Date(),
-//     title: "task1"
-// });
+Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
     Accounts.ui.config({
 	passwordSignupFields: "USERNAME_ONLY"
     });
+    if (Meteor.userId()){
+	Tasks.insert({
+	    owner: Meteor.userId(),
+	    createdAt: new Date(),
+	    title: "task1"
+	});
+	Template.body.helpers({
+	    tasks: function () {
+		return Tasks.find({});
+	    }
+	});
+    }
 }
 
 if (Meteor.isServer) {
