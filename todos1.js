344e81f17a72afe21c2e3b1372dc932af169ad47
Your Name <you@example.com>
2016-03-21 00:06:05 -0400

    Meteor Documentation http://docs.meteor.com/#/basic/Template-events

--- todos1.js
+++ todos1.js
@@ -1,23 +1,47 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+    // counter starts at 0
+    Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+    Template.hello.helpers({
+	counter: function () {
+	    return Session.get('counter');
+	}
+    });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.hello.events({
+	'click button': function () {
+	    // increment the counter when button is clicked
+	    Session.set('counter', Session.get('counter') + 1);
+	}
+    });
+
+    Template.nametag.helpers({
+	// name: "Ben Bitdiddle"
+    });
+
+    Template.welcomePage.helpers({
+	people: [{name: "Bob"}, {name: "Frank"}, {name: "Alice"}]
+    });
+
+    Template.profilePage.helpers({
+	username: function(){
+	    return Meteor.user() && Meteor().username;
+	}
+    });
+
+    Template.post.helpers({
+	commentCount: function (numComments) {
+	    if (numComments === 1) {
+		return "1 comment";
+	    } else {
+		return numComments + " comments";
+	    }
+	}
+    });
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+	// code to run on server at startup
+    });
 }
